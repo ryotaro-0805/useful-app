00d576f4f570a23439665d6659cffe412124ffa0
ryotaro-0805 <rasyouryo@yahoo.co.jp>
2023-02-13 16:01:17 +0900

    🐛 エラー修正のため、データをpubricフォルダへ移動

--- src/pages/components/pipeData.jsx
+++ src/pages/components/pipeData.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-import {pipeDatabase} from '../pipeDatabase'
+import {pipeDatabase} from '../../../public/pipeDatabase'
 
 export default function PipeData() {
     const pipeData = pipeDatabase
