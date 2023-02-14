0412c83bce9d1720e50d0f4494047e08e53afa73
ryotaro-0805 <rasyouryo@yahoo.co.jp>
2023-02-14 11:20:40 +0900

    🐛 バグ修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-import Footer from './components/footer'
+import Footer from '../../public/Footer'
 
 export default function Home() {
   return (
