d4485148f446eedebc675cd8b14f6a2f0387da99
ryotaro-0805 <rasyouryo@yahoo.co.jp>
2023-02-14 10:33:44 +0900

    ✅ 進めました

--- src/pages/components/weightCalculation.jsx
+++ src/pages/components/weightCalculation.jsx
@@ -2,12 +2,20 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useRef, useState } from 'react';
 
 
 export default function WeightCalculation() {
   const router=useRouter();
-  console.log(router.query.size);
-  console.log(router.query.thickness);
+  const ref=useRef();
+  const [result,setResult]=useState(0);
+  const calcFnc=(e)=>{
+    e.preventDefault();
+    setResult(
+      ((router.query.size**2-(router.query.size-router.query.thickness*2)**2)/4*3.141592*ref.current.value*7.85*0.000001).toFixed(1)   
+      )
+  }
+  
   return (
     <>
       <Head>
@@ -19,12 +27,13 @@ export default function WeightCalculation() {
       <main className={styles.main}>
         <div className={styles.description}>
             <h2>重量計算アプリ</h2>
-            <form action="">
+            <form onSubmit={calcFnc} action="">
             <p>{`外径φ${router.query.size}　厚さt${router.query.thickness}の重量計算を行います。`}</p>
             <p>長さを入力してください。</p>
-            <input type="number" /> mm
-            <input type="button" value="決定" />
+            <input ref={ref} type="number" /> mm
+            <input onClick={calcFnc} type="button" value="決定" />
             </form>
+            {result===0?null:<p>計算結果は{result}kgです。</p>}
         </div>
             <Link href='/'><button className={styles.button}>Topへ戻る</button></Link>
       </main>
